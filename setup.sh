a60ceb5e07d0d6329bdc7b0e3927308dd8f94520
Martin <martin.biroscak@gmail.com>
2024-04-04 16:03:25 +0000

    Update setup.sh to use mamba

--- setup.sh
+++ setup.sh
@@ -6,11 +6,20 @@ then
     bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
     rm -rf ~/miniconda3/miniconda.sh
     ~/miniconda3/bin/conda init bash
-    exec bash
+
+    # Source the shell initialization script to apply Conda changes immediately
+    # Adjust the path to the initialization script if necessary
+    source ~/.bashrc
 fi
-conda env create -f environment.yml
-conda activate openfold
-pip install -e ./modules/ESM
-pip install -e ./modules/openfold
+
+if ! command -v mamba &> /dev/null
+then
+    conda install -y -n base --override-channels -c conda-forge mamba 'python_abi=*=*cp*'
+fi
 
 
+# Now that Conda is initialized in the current session, you can proceed with the rest
+mamba env create -y environment.yml
+mamba activate leonoviny_env
+# pip install -e ./modules/ESM
+# pip install -e ./modules/openfold
